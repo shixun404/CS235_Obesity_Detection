19f450cd72f4e03dd609d4801a6d170765e598db
shixun404 <shixun404@gmail.com>
2022-12-05 03:07:11 -0800

    update CART_decision_tree, accu = 81.43%

--- CART_decision_tree.py
+++ CART_decision_tree.py
@@ -7,7 +7,7 @@ from sklearn.metrics import recall_score
 from sklearn.metrics import f1_score
 from sklearn.metrics import confusion_matrix
 
-def gini(x,y):
+def greedy_split_gini(x,y):
     # condition dictionary
     gini = 0
     df = x.join(y)
@@ -15,33 +15,34 @@ def gini(x,y):
     label_value = None
     for label in x.columns:
         label_stat = df[label].value_counts()
-        # print(label, tmp)
-        # print(label, df[label].value_counts().shape)
-        predict = df['Label'].value_counts()
         s = label_stat.sum()
         for key in label_stat.index:
-            value = label_stat[key]
-            tmp = ((df[df[label] <= value])['Label']).value_counts()
-            p = tmp.values[0] / tmp.sum()
-            gini = value / s * (1 - p ** 2 - (1-p) ** 2)
+            tmp = ((df[df[label] <= key])['Label']).value_counts()
+            if tmp.size > 0:
+                value = tmp.sum()
+                p = tmp.values[0] / tmp.sum()
+                gini = value / s * (1 - p ** 2 - (1-p) ** 2)
+            tmp = ((df[df[label] > key])['Label']).value_counts()
+            if tmp.size > 0:
+                p = tmp.values[0] / tmp.sum()
+                value = tmp.sum()
+                gini += value / s * (1 - p ** 2 - (1-p) ** 2)
         if gini < min_gini:
-                label_ = label
-                label_value = key
-                predict_ = predict
-                min_gini = tmp
-    a_1 = 0
-    a_2 = 0
-    for key in predict.index:
-        if key == 0.0:
-            a_1 = predict.loc[key]
-        else:
-            a_2 = predict.loc[key]
-    if a_1 > a_2:
-        predict = False
-    else:
-        predict = True
-    return gini, predict
-        
+            label_ = label
+            label_value = key
+            min_gini = gini
+    return label_, label_value, min_gini
+a_1 = 0
+a_2 = 0
+# for key in predict.index:
+#         if key == 0.0:
+#             a_1 = predict.loc[key]
+#         else:
+#             a_2 = predict.loc[key]
+#     if a_1 > a_2:
+#         predict = False
+#     else:
+#         predict = True
 class Node:
     def __init__(self,):
         self.child = []
@@ -50,7 +51,7 @@ class Node:
         self.gini = 0
         self.label_value = None
 
-def build_dfs(x, y, parent_gini, label_value):
+def build_dfs(x, y):
     node = Node()
     min_gini = 100
     label_ = None
@@ -67,13 +68,9 @@ def build_dfs(x, y, parent_gini, label_value):
         predict_no_child = False
     else:
         predict_no_child = True
-    # print(x.join(y))
-    # print("####################################")
     p = tmp.values[0] / tmp.sum()
     gini_no_child = (1 - p ** 2 - (1-p) ** 2)
-   
-    # assert 0
-    # print(parent_gini, min_gini)
+    label_, label_value, min_gini = greedy_split_gini(x,y)
     if gini_no_child <= min_gini:
         node.label = label_
         node.predict = predict_no_child
@@ -82,24 +79,20 @@ def build_dfs(x, y, parent_gini, label_value):
         return node
     else:
         node.label = label_
-        node.predict = predict_
+        node.predict = None
         node.gini = min_gini
         node.label_value = label_value
-        # print( label_, x[label_].unique().shape)
-        for i in x[label_].unique():
-            index = x.index[x[label_] == i]
-            # print(index)
-            # print(x.loc[index].drop(columns=[label_]))
-            # assert 0
-            child_node = build_dfs(x.loc[index].drop(columns=[label_]), y.loc[index], min_gini, i)
-            # print(child_node)
-            if child_node != None:
-                node.child.append(child_node)
+        index = x.index[x[label_] <= label_value]
+        child_node = build_dfs(x.loc[index], y.loc[index])
+        node.child.append(child_node)
+        index = x.index[x[label_] > label_value]
+        child_node = build_dfs(x.loc[index], y.loc[index])
+        node.child.append(child_node)
     return node
-b = 3
+
 def preprocessing() -> pd.DataFrame:
     df = pd.read_csv('ObesityDataSet_raw_and_data_sinthetic.csv')
-    df = df.loc[:10 * b]
+    # df = df.loc[:10 * b]
     df['Label'] = df['NObeyesdad'].str.startswith('Obesity').astype(int)
     del df['NObeyesdad']
     del df['Height']
@@ -109,10 +102,10 @@ def preprocessing() -> pd.DataFrame:
     'Frequently': 2, 'Always': 3}
 
     yes_no_map = {'yes': 1, 'no': 0}
-    df['Age'] = (df['Age'] / 10).round() * 10
-    df['CH2O'] = (df['CH2O'] * 2).round() / 10
-    df['FAF'] = (df['FAF'] * 2).round() / 10
-    df['TUE'] = (df['TUE'] * 2).round() / 10
+    df['Age'] = (df['Age']).round()
+    df['CH2O'] = (df['CH2O'] * 10).round() / 10
+    df['FAF'] = (df['FAF'] * 10).round() / 10
+    df['TUE'] = (df['TUE'] * 10).round() / 10
     df['FCVC'] = df['FCVC'].round()
     df['Gender'] = df['Gender'].map({'Male': 1, 'Female': 0})
     df['family_history_with_overweight'] = df['family_history_with_overweight'].map(yes_no_map)
@@ -122,8 +115,6 @@ def preprocessing() -> pd.DataFrame:
     df['SCC'] = df['SCC'].map(yes_no_map)
     df['CALC'] = df['CALC'].map(freq_map)
     df['MTRANS'] = df['MTRANS'].map({'Automobile': 0, 'Motorbike': 0, 'Public_Transportation': 0, 'Walking': 1, 'Bike': 1})
-    # print(df)
-    # print(df.max()-df.min()  +1e-7)
     normalized_df = (df-df.min())/(df.max()-df.min() + 1e-7)
     df = normalized_df
     return df
@@ -132,35 +123,23 @@ df = preprocessing()
 X = df.drop(columns=['Label'])
 y = df['Label']
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.1, random_state=0)
-X_train = X.iloc[:9*b]
-y_train = y.iloc[:9*b]
-X_test = X.iloc[9*b:]
-y_test = y.iloc[9*b:]
-# X_train = X.iloc[:45]
-# y_train = y.iloc[:45]
-# X_test = X.iloc[45:]
-# y_test = y.iloc[45:]
-# print(y_test.shape, y_train.shape)
-# assert 0
-node = build_dfs(X_train, y_train, 100,0)
-# node = build_dfs(X, y, 100,0)
-# assert 0
+
+node = build_dfs(X_train, y_train)
+
 k =0
-# for i in range(X_test.shape[0]):
+
 cnt = 0
 for i in range(X_test.shape[0]):
     node_ = node
     while (len(node_.child) > 0):
         # print(node_.label, len(node_.child))
         k+=1
-        if k > 20:
-            break
-        for j in range(len(node_.child)):
+        if len(node_.child) > 0:
             # print(j,node_.label, node_.child[j].label_value, (X.iloc[i])[node_.label])
-            if node_.child[j].label_value == (X_test.iloc[i])[node_.label]:
-                # print("Choose! ",f"{j}-th  child,",node_.label, node_.child[j].label_value, (X.iloc[i])[node_.label])
-                node_ = node_.child[j]
-                break
+            if (X_test.iloc[i])[node_.label] <= node_.label_value:
+                node_ = node_.child[0]
+            else:
+                node_ = node_.child[1]
             # print(j, node_.child[j].label_value, (X_test.iloc[i])[node_.label])
             
     print(f"{i}-th row is obesity: {node_.predict}, {y_test.iloc[i]}")
